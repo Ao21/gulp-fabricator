078992ac88ff86c67d667f0a4282b1c4d0bedaa3
Brett <Ronan.Brett@theaa.ie>
2015-09-08 17:19:18 +0100

    added functionality to json extracting

--- index.js
+++ index.js
@@ -8,14 +8,31 @@ var dss = require('dss');
 
 function plugin(opts) {
 
-	console.log(opts);
+	console.log('options: ', opts);
 
 	function transform (file, enc, cb) {
-		dss.parse(file.contents.toString(), {}, function(dssFile) {
-			console.log(dssFile);
-			file.contents = dssFile;
-			cb(null, file);
+		dss.parser('version', function (e, text) {
+			return text;
 		})
+		dss.parse(file.contents.toString(), {}, function(dssFile) {
+
+			//file.contents = dssFile;
+			if (dssFile.blocks.length > 0) {
+				var dss = JSON.stringify(dssFile);
+				file.contents = new Buffer(dss);
+				file.path = file.path.slice(0,-4) + 'json';
+			}
+			else {
+				file = null;
+			}
+			
+			cb(null, file);
+			
+		})
+	}
+	
+	function createStylesArray(styles, cb) {
+		
 	}
 
 	function sendToUILibrary(file) {
@@ -25,8 +42,8 @@ function plugin(opts) {
 	function callURL(url, obj) {
 		// Call URL
 	}
+	
 
-  	
   	return through.obj(transform)
   
 }
